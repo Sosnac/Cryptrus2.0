0be356cc6415a799f62537480d4cbda06a35df67
David Sosnac <105160225+Sosnac@users.noreply.github.com>
2026-04-01 00:40:46 +0200

    Update cryptrus2.0.py

--- cryptrus2.0.py
+++ cryptrus2.0.py
@@ -29,7 +29,7 @@ except ImportError as e:
 
 # Initialize them
 scanner = NetworkScanner()
-detector = DeviceDetection()
+detector = DeviceDetector()
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
