bb305593fcfc7f7d28c15721c11e71ac3f3e17e0
David Sosnac <105160225+Sosnac@users.noreply.github.com>
2026-03-31 13:57:34 +0200

    Create device_detection.py

---
+++ device_detection.py
@@ -0,0 +1,20 @@
+class DeviceDetection:
+    """Analyzes network data to identify device types."""
+
+    def __init__(self):
+        # Example OUI (Organizationally Unique Identifier) mapping
+        self.vendor_map = {
+            "B8:27:EB": "Raspberry Pi Foundation",
+            "00:04:20": "Slim Devices",
+            "00:0C:29": "VMware Virtual Machine"
+        }
+
+    def identify_by_mac(self, mac_address):
+        """
+        Identifies a device vendor based on the MAC address.
+        """
+        if not mac_address:
+            return "Unknown Device"
+            
+        prefix = mac_address.upper()[:8].replace("-", ":")
+        return self.vendor_map.get(prefix, "Generic Network Device")
