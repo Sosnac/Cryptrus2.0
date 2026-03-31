a736bb1b3a0b2bec0154d582b0759c51086058bd
David Sosnac <105160225+Sosnac@users.noreply.github.com>
2026-03-31 14:21:41 +0200

    Update cryptrus2.0.py

--- cryptrus2.0.py
+++ cryptrus2.0.py
@@ -15,6 +15,9 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
+from steganography import SteganoProvider
+from auth_2fa import Auth2FA
+
 import argparse
 import json
 import subprocess
